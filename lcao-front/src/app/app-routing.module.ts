6b80c551f41d3603ce88a566befaa6b7346a9e6e
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-05-16 10:15:32 -0300

    correção de rota no header + estilos em cadastro-usuario atualizados

--- lcao-front/src/app/app-routing.module.ts
+++ lcao-front/src/app/app-routing.module.ts
@@ -1,48 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListarLembreteComponent } from './pages/listar-lembrete/listar-lembrete.component';
-import { CriarLembreteComponent } from './pages/criar-lembrete/criar-lembrete.component';
-import { ExcluirLembreteComponent } from './pages/excluir-lembrete/excluir-lembrete.component';
-import { EditarLembreteComponent } from './pages/editar-lembrete/editar-lembrete.component';
-import { LoginUsuarioComponent } from './pages/login-usuario/login-usuario.component';
+
 import { CadastrarUsuarioComponent } from './pages/cadastrar-usuario/cadastrar-usuario.component';
+import { CriarLembreteComponent } from './pages/criar-lembrete/criar-lembrete.component';
+import { EditarLembreteComponent } from './pages/editar-lembrete/editar-lembrete.component';
+import { ExcluirLembreteComponent } from './pages/excluir-lembrete/excluir-lembrete.component';
+import { ListarLembreteComponent } from './pages/listar-lembrete/listar-lembrete.component';
+import { LoginUsuarioComponent } from './pages/login-usuario/login-usuario.component';
 import { SairUsuarioComponent } from './pages/sair-usuario/sair-usuario.component';
 
 const routes: Routes = [
   {
-  path: '',
-  redirectTo: 'listarLembrete',
-  pathMatch: 'full'
-},
-{
-  path: 'listarLembrete',
-  component: ListarLembreteComponent
-},
-{
-  path: 'criarLembrete',
-  component: CriarLembreteComponent
-},
-{
-  path: 'lembretes/excluirLembrete/:id',
-  component: ExcluirLembreteComponent
-},
-{
-  path: 'lembretes/editarLembrete/:id',
-  component: EditarLembreteComponent
-},
-{
-  path: 'login',
-  component: LoginUsuarioComponent
-},
-{
-  path: 'cadastrarUsuario',
-  component: CadastrarUsuarioComponent
-},
-{
-  path: 'sairUsuario',
-  component: SairUsuarioComponent
-}
-
+    path: 'listarLembrete',
+    component: ListarLembreteComponent
+  },
+  {
+    path: 'criarLembrete',
+    component: CriarLembreteComponent
+  },
+  {
+    path: 'lembretes/excluirLembrete/:id',
+    component: ExcluirLembreteComponent
+  },
+  {
+    path: 'lembretes/editarLembrete/:id',
+    component: EditarLembreteComponent
+  },
+  {
+    path: 'login',
+    component: LoginUsuarioComponent
+  },
+  {
+    path: 'cadastrarUsuario',
+    component: CadastrarUsuarioComponent
+  },
+  {
+    path: 'sair',
+    component: SairUsuarioComponent
+  },
+  {
+    path: '',
+    redirectTo: 'listarLembrete',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
